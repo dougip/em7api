54b77af1f323faed248403bc8e22924cf7c91a2d
Doug Ip <dougip@gmail.com>
2019-05-02 22:23:23 +0200

    add README to pypi description

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ with open('README.rst') as f:
 setup(
   name='em7api',
   author='Doug Ip',
-  version='0.1.4',
+  version='0.1.5',
   packages=['em7api',],
   package_dir={'em7api': 'em7api'},
+  description='An unofficial Python wrapper for the Science Logic EM7 API',
   long_description=long_description_contents,
   install_requires=['requests']
 )
